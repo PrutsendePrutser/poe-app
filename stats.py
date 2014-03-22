70eff5c49575257e8fc4b261a6c3e3509b169294
baarts <bmtc.aarts@gmail.com>
2014-03-22 02:28:14 +0100

    Added class variables for added damage, attack speed and crit
    Added class variables to the stats.py module to store mods on gear
    related to increasing damage

--- stats.py
+++ stats.py
@@ -4,6 +4,34 @@ import json
 
 class items(object):
     """This class contains functions related to equipment"""
+    
+    def __init__(self):
+        # List that contains the itemslots
+        self.itemslots = []
+        
+        # Total added physical damage from gear for the current character
+        self.addedphysicaldamage = 0
+        
+        # Total added lightning damage from gear for the current character
+        self.addedlightningdamage = 0
+        
+        # Total added cold damage from gear for the current character
+        self.addedcolddamage = 0
+        
+        # Total added fire damage from gear for the current character
+        self.addedfiredamage = 0
+        
+        # Total added chaos damage from gear
+        self.addedchaosdamage = 0
+        
+        # Total added attack speed from gear
+        self.addedatkspeed = 0
+        
+        # Total added critical strike chance from gear
+        self.addedcritchance = 0
+        
+        # Total added critical strike multiplier from gear
+        self.addedcritmulti = 0
 
     def importItems(self):
         """The importItems function loads a file that contains equipment/inventory of a character.
