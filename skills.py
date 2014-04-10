adc820231c7337e5a1c650292f3a7db79b81cfd0
baarts <bmtc.aarts@gmail.com>
2014-04-10 14:12:36 +0200

    Added HTML parsing to skills.py
    Added code to skills.py to parse the HTML and format the required data.
    The data is printed to a csv file in tab-delimited format

--- skills.py
+++ skills.py
@@ -5,6 +5,7 @@ with their stats and information
 
 import httplib
 import HTMLParser
+import time
 
 class Skillgems(object):
     
@@ -12,47 +13,83 @@ class Skillgems(object):
     def __init__(self):
         self.active_skills = {}
         self.support_skills = {}
+        self.skilldata = {}
+        self.counter = 0
         
-    def readskillpage(self, skillname):
+    def readskillpage(self, skillname, activeskillfile):
         parser = MyHTMLParser()
         skillname = "/{}".format(skillname.replace(" ", "_"))
-        print skillname
         conn = httplib.HTTPConnection("pathofexile.gamepedia.com")
+        print "http://pathofexile.gamepedia.com%s" % skillname
         conn.request("GET", skillname)
         response = conn.getresponse()
-        print response.status, response.reason
         if response.status == 200:
-            print "200"
             parser.feed(response.read())
-            
+            print parser.d
+            skilln = skillname.replace('/', '').replace('_', ' ')
+            print skilln, self.skilldata.keys()
+            self.skilldata[skilln] = [l for l in parser.d.split('\n') if l.strip()]
+        else:
+            print response.status, response.reason
+        print self.counter
+        self.counter += 1
+        conn.close()
+        time.sleep(30)
+        self.writeskillfile(skilln, activeskillfile)
+    
+    def writeskillfile(self, skill, activeskillfile):
+        activeskillfile.write(skill.replace('/', '').replace('_', ' ') + '\t' + self.skilldata[skill][0] + '\n')
+        for level in self.skilldata[skill][1:]:
+            # Ensure we only go to lvl 20
+            if int(level.split('\t')[0]) > 20:
+                break
+            activeskillfile.write('\t' + str(level) + '\n')
+        
 class MyHTMLParser(HTMLParser.HTMLParser):
     inskilltable = False
     d = ""
+    prev_tag = ""
     append_data = False
     def handle_starttag(self, tag, attrs):
         if tag == "table" and ("class", "wikitable GemLevelTable") in attrs:
             self.inskilltable = True
         if tag == "tr":
             self.d += '\n'
+        if tag == 'td':
+            self.prev_tag = "td"
             
     def handle_endtag(self, tag):
         if tag == "table" and self.inskilltable:
             self.inskilltable = False
-            print self.d
+            self.d +='\n'
         if tag == "tr" and self.d:
             pass
     def handle_startendtag(self, tag, attrs):
-        pass
+        if self.inskilltable:
+            if tag == 'br' and self.prev_tag == "td":
+                self.d = self.d.rstrip('\t') + " "
+            if tag == 'img':
+                if 'Level_up_icon_small' in attrs[1][1]:
+                    self.d += 'Required level\t'
+                elif "StrengthIcon" in attrs[1][1]:
+                    self.d += 'Strength\t'
+                elif "DexterityIcon" in attrs[1][1]:
+                    self.d += "Dexterity\t"
+                elif "IntelligenceIcon" in attrs[1][1]:
+                    self.d += "Intelligence\t"
     def handle_data(self, data):
         if self.inskilltable:
-            val = data.replace('\n','').replace('\r','').replace('\t','').replace('<br />','').rstrip()
-            self.d += val + '\t'
+            val = data.replace('\n','').replace('\r','').replace('\t','').replace('<br />','').strip()
+            if val:
+                self.d += val.strip() + '\t'
 
 gems = Skillgems()
 skillist = []
-with open('skills.txt', 'r') as skillfile:
-    skilllist = skillfile.readlines()
-skilllist = [skill.replace('\r', '').strip() for skill in skilllist]
-print skilllist
-for skill in skilllist:
-    gems.readskillpage(skill)
+skillfiles = ['skills.txt', 'skills2.txt', 'skills3.txt', 'skills4.txt', 'skills5.txt', 'skills6.txt']
+for f in skillfiles:
+    with open(f, 'r') as skillfile:
+        skilllist = skillfile.readlines()
+    skilllist = [skill.replace('\r', '').strip() for skill in skilllist]
+    with open('active_skill_levels.csv', 'a') as activeskillfile:
+        for skill in skilllist:
+            gems.readskillpage(skill, activeskillfile)
