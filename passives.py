31685a7e191ad3f1c00cdc53b9f308cbe1afbc3b
baarts <bmtc.aarts@gmail.com>
2014-04-14 16:21:31 +0200

    Add function load_node_data to passives.py
    Added the function load_node_data to the passives.py module. This
    function currently only retrieve the node IDs of all the nodes in the
    passive tree. However, later this function will retrieve all the
    relevant information of the skill nodes, to support calculation of
    bonuses from a given passive tree

--- passives.py
+++ passives.py
@@ -1,4 +1,5 @@
 import base64
+import json
 
 class PassiveCalculator(object):
     
@@ -26,8 +27,23 @@ class PassiveCalculator(object):
             # Combine the two characters, and decode them using utf-16 decoding
             # Cast to an ord so we have the 16 bit int value
             # Add to the node_ids list
-            node_ids.append(ord((build[i+1] + build[i+0]).decode("utf-16")))
-        
+            print ((i-6) / 2), (build[i+1] + build[i+0])
+                
+            try:
+                c = (build[i+1] + build[i+0]).decode("utf-16")
+                if c == "":
+                    c = "  "
+                node_ids.append(ord(c))
+            except UnicodeDecodeError:
+                print "Error: ", (build[i+1] + build[i+0])
+            except TypeError:
+                print "C: ", repr(c)
+            
+            
         # When we have parsed the full build url, return the node_ids
         return node_ids
+    
+    def load_nodes_data(self):
+        with open("skilltree.txt", "r") as sfile:
+            return json.loads(sfile.read())
 
