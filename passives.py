36457491e3e184610e26676a26fba5e680639ec9
baarts <bmtc.aarts@gmail.com>
2014-04-14 14:44:58 +0200

    Add init function and function to get node IDs from URL to passives.py
    Added an init function to passives.py that sets the build URL that the
    user has entered. Also added a function to base64 decode the build URL
    and a function to retrieve the node IDs of the skilled nodes.
    
    The function that does the base64 decoding returns a string, the
    function that retrieves the node IDs of the skilled nodes returns a list
    of integers.

---
+++ passives.py
@@ -0,0 +1,33 @@
+import base64
+
+class PassiveCalculator(object):
+    
+    def __init__(self, build_url):
+        """Initialize class instance"""
+        # Set the build url that the user entered
+        self.build_url = build_url
+        
+    def load_from_url(self):
+        # Make a local build URL so we don't change the original one
+        local_build_url = self.build_url.replace("-", "+").replace("_", "/")
+        
+        # Base 64 decode the build URL and return it
+        return base64.b64decode(local_build_url)
+    
+    def convert_to_node_ids(self, build):
+        # Make a list to store the node IDs
+        node_ids = []
+        
+        # The actual build nodes start at the seventh index
+        # Loop over the build from the first node, in steps of 2
+        for i in range(6, len(build), 2):
+            # In the base64 decoded string, each node ID is created out of 2 decoded characters
+            # Where i+1 is the first one of the two and i is the second of the two
+            # Combine the two characters, and decode them using utf-16 decoding
+            # Cast to an ord so we have the 16 bit int value
+            # Add to the node_ids list
+            node_ids.append(ord((build[i+1] + build[i+0]).decode("utf-16")))
+        
+        # When we have parsed the full build url, return the node_ids
+        return node_ids
+
