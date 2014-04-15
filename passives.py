e005ce353ee051111881248b92d2ffcd55f2c3bf
baarts <bmtc.aarts@gmail.com>
2014-04-15 12:10:11 +0200

    Update function load_node_data in passive.py
    Updated the load_node_data function in the passives.py module so that
    the function now loads the contents of the file that contains the node
    IDs, node names and descriptions/bonuses of all the nodes in the tree.

--- passives.py
+++ passives.py
@@ -6,7 +6,13 @@ class PassiveCalculator(object):
     def __init__(self, build_url):
         """Initialize class instance"""
         # Set the build url that the user entered
-        self.build_url = build_url
+        self.build_url = build_url.split("passive-skill-tree/")[1]
+        self.selected_node_ids = self.convert_to_node_ids(self.load_from_url())
+        self.node_data = self.load_nodes_data()
+    
+    def set_build_url(self, url):
+        self.build_url = url.split("passive-skill-tree/")[1]
+        self.selected_node_ids = self.convert_to_node_ids(self.load_from_url())
         
     def load_from_url(self):
         # Make a local build URL so we don't change the original one
@@ -44,6 +50,15 @@ class PassiveCalculator(object):
         return node_ids
     
     def load_nodes_data(self):
-        with open("skilltree.txt", "r") as sfile:
-            return json.loads(sfile.read())
+        with open("skillnodes.txt", "r") as sfile:
+            lines = sfile.readlines()
+            node_dict = {}
+            for line in lines:
+                split = line.split('\t')
+                nodeid = split[0]
+                name = split[1]
+                desc = split[-1]
+                node_dict[nodeid] = {'name': name,
+                                     'desc': desc}
+            return node_dict
 
