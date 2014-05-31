ed02a7f222ba7faaa50585a8fc9704893957e022
PrutsendePrutser <ijsbeer.aarts@gmail.com>
2014-05-31 15:56:22 +0200

    Add functions related to parsing items file to itemstats.py
    Added empty functions related to parsing items file to the itemstats.py
    module. The idea is that this module returns a dictionary that contains
    the offensive and defensive bonuses that are aquired from gear, as well
    as the equipped skill gems/links per item slot.

--- itemstats.py
+++ itemstats.py
@@ -4,3 +4,15 @@ class ItemStats(object):
     
     def __init__(self):
         pass
+    
+    def load_items_file(self):
+        pass
+    
+    def extract_offensive_bonuses(self):
+        pass
+    
+    def extract_defensive_bonuses(self):
+        pass
+    
+    def get_equipped_skill_gems(self):
+        pass
