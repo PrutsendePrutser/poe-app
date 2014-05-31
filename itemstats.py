95c3996c8d8218cf071348bcb2ab047cac808a17
PrutsendePrutser <ijsbeer.aarts@gmail.com>
2014-05-31 16:08:15 +0200

    Fill function load_items_file
    Added code to the load_items_file function to read and parse the items
    file. Also added a class variable for the item slots.

--- itemstats.py
+++ itemstats.py
@@ -2,11 +2,17 @@ import json
 
 class ItemStats(object):
     
-    def __init__(self):
-        pass
-    
-    def load_items_file(self):
-        pass
+    def __init__(self, path):
+        # Item slots
+        self.item_slots = set(['Helm', 'BodyArmour', 'Weapon', 'Weapon2', 'Offhand', 'Gloves', 'Boots', 'Amulet', 'Ring', 'Ring2', 'Belt'])
+        
+        # Retrieve a dictionary that contains all the items that are on the character
+        self.items_dict = self.load_items_file(path)
+        
+    def load_items_file(self, path):
+        # Open the file in the given path, read the file and convert the JSON to a dictionary.
+        with open(path, 'r') as itemsfile:
+            return json.loads(itemsfile.read())
     
     def extract_offensive_bonuses(self):
         pass
@@ -16,3 +22,5 @@ class ItemStats(object):
     
     def get_equipped_skill_gems(self):
         pass
+    
+ItemStats('files/items.txt')
