c1f14bb8db606246a3d7fe3589bf4e9a1e91375b
PrutsendePrutser <ijsbeer.aarts@gmail.com>
2014-06-20 23:30:56 +0200

    Add support for socket/link groups when retrieving socketed gems
    Added support for socket/link groups when retrieving the gems socketed
    in an item. This is crucial for correct dps calculation. Also added some
    documentation to explain what the code does.

--- itemstats.py
+++ itemstats.py
@@ -16,18 +16,57 @@ class ItemStats(object):
         self.get_item_info()
     
     def get_item_info(self):
+        
+        # Loop over the items in the inventory
         for i in self.items_dict['items']:
+            
+            # Retrieve the inventory ID so we can filter only for equipped gear
             inv_id = i['inventoryId']
+            
+            # If it's an actual equipped item slot
             if inv_id in self.item_slots:
-                #for k in i.keys():
-                #    print k
+                
+                # Create a dictionary for socket groups with the links for each group
+                socket_groups = {}
+                
+                # Loop over the socket attribute of the items JSON
+                for group in i['sockets']:
+                    if group['group'] in socket_groups.keys():
+                        # Add the socket color to the list value, this is used to calculate the number of links/bonuses
+                        socket_groups[group['group']].append(group['attr'])
+                    else:
+                        # Add a new list value
+                        socket_groups[group['group']] = [group['attr']]
+                
+                # Retrieve the gems that are equipped in the item
+                slotted_gems = self.get_equipped_skill_gems(i)
+                
+                # Create a dictionary entry for the current equipment slot
                 self.item_info[inv_id] = {}
-                self.item_info[inv_id]['slotted_gems'] = self.get_equipped_skill_gems(i)
-        print self.item_info
-        
-        for c in self.items_dict['character']:
-            print self.items_dict['character'][c]
-        
+                
+                # Counter to put the correct skillgems in the correct link group
+                counter = 0
+                
+                # Loop over the socket groups
+                for group in sorted(socket_groups.keys()):
+                    # Create an entry for the current socket group
+                    self.item_info[inv_id][str(group)] = {}
+                    
+                    # Get the number of links for the current socket group
+                    link_counter = len(socket_groups[group])
+                    
+                    # Add the socketed gems in the current link group
+                    self.item_info[inv_id][str(group)]['slotted_gems'] = slotted_gems[counter:counter+link_counter]
+                    
+                    # Update the counter so we know which skill gem in the list to take next.
+                    counter += link_counter
+                    
+        # Loop over the equipped items
+        for item in self.item_info:
+            # Loop over the socket groups
+            for socket_group in self.item_info[item]:
+                # Print the socketed gems for each socket group of each item
+                print item, socket_group, self.item_info[item][socket_group]
     
     def load_items_file(self, path):
         # Open the file in the given path, read the file and convert the JSON to a dictionary.
@@ -41,25 +80,38 @@ class ItemStats(object):
         pass
     
     def get_equipped_skill_gems(self, item):
-        equipped_skills = {}
-        #print self.items_dict['items']
+        # List of the skills equipped in the item
+        equipped_skills = []
+        
+        # Socketed items attribute in the item JSON
         socketed_items = item['socketedItems']
+        
+        # If there are no socketed items this should be empty
         if not socketed_items:
             return
+        # If there are socketed gems, loop over the socketed gems
         for i in socketed_items:
-            skill_name = i['typeLine']
-            equipped_skills[skill_name] = {}
-            #print skill_name
-            for prop in i['properties']:
-                if prop['values']:
-                    equipped_skills[skill_name][prop['name']] = prop['values'][0][0]
-                else:
-                    equipped_skills[skill_name]['keywords'] = prop['name'].split(', ')
-            #print '\n'
-        return equipped_skills
             
-        
-        #for socketedItem in self.items_dict['items'][0][item_slot]['socketedItems']:
-            #print socketedItem
+            # Get the skill name
+            skill_name = i['typeLine']
+            
+            # Create the skill list
+            skill_list = [skill_name]
+            
+            # Loop over the gem properties
+            for prop in i['properties']:
+                
+                # If there's a value
+                if prop['values']:
+                    # Add the value and name to the skill list
+                    skill_list.append([prop['values'][0][0], prop['name']])
+                else:
+                    # If these are the keywords, add a list of keywords
+                    skill_list.append(prop['name'].split(', '))
+            
+            # Add the skill to the equipped skills list for this item
+            equipped_skills.append(skill_list)
+        return equipped_skills
+    
     
 ItemStats('files/items.txt', 'PrutsMarauder')
